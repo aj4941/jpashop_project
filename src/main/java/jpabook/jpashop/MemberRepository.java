376f5cfcbffd29aa39a148dabce4ae8c38b5e742
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-24 16:03:50 +0900

    MemberRepository 세팅 후 테스트

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // 스프링 컨테이너에서 엔티티 매니저 주입
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId(); // 저장을 하고나면 Id 정도만 반환하도록 설계
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
