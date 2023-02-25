7186849790d19b136de794fa91c2febde219381c
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-25 22:12:34 +0900

    Category와 Item간 연관관계 매핑 오류 수정

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -18,10 +18,12 @@ public class Category {
 
     private String name;
 
-    @ManyToMany(mappedBy = "categories")
-    @JoinTable(name = "category_item",
-            joinColumns = @JoinColumn(name = "category"),
-            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    @ManyToMany
+    @JoinTable(name = "category_item", // 연관관계의 주인을 category로 잡음
+            joinColumns = @JoinColumn(name = "category_id"), // category 쪽으로 들어가는 컬럼
+            inverseJoinColumns = @JoinColumn(name = "item_id")) // item 쪽으로 들어가는 컬럼
+    // 중간 테이블 매핑 (일대다, 다대일로 풀어내기 위함)
+    // 실무에서는 매핑으로 끝나는 것이 아니라 추가 정보를 중간 테이블에 넣는 경우가 있어서 선호하지 않는 방식이다.
     private List<Item> items = new ArrayList<>();
 
     @ManyToOne // 부모는 1개이므로 ManyToOne으로 설정
--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -22,12 +22,7 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-    @ManyToMany
-    @JoinTable(name = "category_item", // 연관관계의 주인을 category로 잡음
-            joinColumns = @JoinColumn(name = "category"), // category 쪽으로 들어가는 컬럼
-            inverseJoinColumns = @JoinColumn(name = "item_id")) // item 쪽으로 들어가는 컬럼
-    // 중간 테이블 매핑 (일대다, 다대일로 풀어내기 위함)
-    // 실무에서는 매핑으로 끝나는 것이 아니라 추가 정보를 중간 테이블에 넣는 경우가 있어서 선호하지 않는 방식이다.
+    @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
 }
