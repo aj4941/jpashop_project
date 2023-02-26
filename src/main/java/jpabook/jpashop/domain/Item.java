937c7c969ecce93a478fa7d15fbcdefb53f10e58
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-26 12:19:29 +0900

    엔티티 내부에 비즈니스 로직 설계 및 예외 만들기

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.catalina.LifecycleState;
@@ -25,4 +26,16 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 엔티티 안에서 비즈니스 로직 설계
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity; // 재고 수량 증가
+    }
+
+    public void removeStock(int quantity) { // 재고 수량 감소
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
