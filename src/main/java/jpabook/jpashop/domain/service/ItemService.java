62eadb0681247ddc4073ed19ca82a1d29982416c
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-26 20:54:51 +0900

    ItemService 작성

---
+++ src/main/java/jpabook/jpashop/domain/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.domain.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // 여기서는 readOnly가 적용이 안됨
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
