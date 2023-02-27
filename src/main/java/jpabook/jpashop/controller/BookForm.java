68de4b82451a524082c3fdf1ad89c0553aa24334
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-27 21:14:50 +0900

    Item 수정내용

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -1,4 +1,16 @@
 package jpabook.jpashop.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class BookForm {
+
+    // 상품의 공통 속성
+    private Long id;
+    private String name;
+    private int price, stockQuantity;
+
+    // 책과 관련된 특별한 속성
+    private String author, isbn;
 }
