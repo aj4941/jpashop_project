f69cdf99f2cc1d491009f3b8d120e3ec7edfadcc
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-02-27 15:11:02 +0900

    회원가입 메서드 create 생성

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +22,22 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    // @Valid : MemberForm에 있는 검증 애노테이션을 확인 (@NotEmpty)
+    // BindingResult가 있으면 오류가 발생해도 오류가 담겨서 실행이 됨
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm"; // 오류가 발생하면 다시 로그인 창으로 이동
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName()); member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/"; // html이 아닌 url로 이동 (redirect)
     }
 }
