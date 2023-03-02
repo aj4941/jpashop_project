35478b56bf128d57635ba816330f745c67e2160c
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-02 17:25:31 +0900

    ordersV6 작성

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -76,4 +76,16 @@ public class OrderQueryRepository {
 
         return result;
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new" +
+                        " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" + // join 조건이 없으면 열의 값이 같은 레코드만 얻음
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
